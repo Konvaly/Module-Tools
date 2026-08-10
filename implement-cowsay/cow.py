6727ccde298826da6b7b9765ab8665b0df21a7eb
Konvaly <konvaly@gmail.com>
2026-08-10 23:13:43 +0100

    Implement cowsay in Python

---
+++ implement-cowsay/cow.py
@@ -0,0 +1,20 @@
+import argparse
+import cowsay
+parser = argparse.ArgumentParser(
+    prog="cowsay",
+    description="Make animals say things",
+)
+parser.add_argument(
+    "--animal",
+    choices=cowsay.char_names,
+    default="cow",
+    help="The animal to be saying things.",
+)
+parser.add_argument(
+    "message",
+    nargs='+',
+    help="The message to say.",
+)
+args = parser.parse_args()
+message = " ".join(args.message)
+print(cowsay.get_output_string(args.animal, message))
